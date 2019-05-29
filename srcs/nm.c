f5ad6d94356b84cfdfe0fc4a20d3208434f1fb78
Victor LISTRAT <vlistrat@e1r6p3.42.fr>
2019-05-29 16:36:39 +0200

    mep nm

--- srcs/nm.c
+++ srcs/nm.c
@@ -1,6 +1,16 @@
 #include "nm.h"
 
+void    ft_nm(char *arg)
+{
+    void    *ptr;
+
+    
+}
+
 int     main(int ac, char **av)
 {
-
+    if (ac < 2)
+        return (0);
+    ft_nm(av[1]);
+    return (0);
 }
